a63b8e866f2b11deec5a7c79f72b4d9f92cd7b0b
jcorrear468 <jcorrear468@gmail.com>
2024-10-13 05:01:33 +0000

    add panas

--- src/person/api/index.ts
+++ src/person/api/index.ts
@@ -11,7 +11,21 @@ export class Person implements IPerson {
 
 
 export class PersonManager implements IPersonManager {
-    private persons: IPerson[] = []
+    private persons: IPerson[] = [
+        new Person('', 'Titon', new Date(2001, 0, 15), ''),
+        new Person('', 'MaDmA', new Date(2002, 1, 4), ''),
+        new Person('', 'Mati', new Date(2001, 2, 8), ''),
+        new Person('', 'Fede', new Date(2002, 6, 16), ''),
+        new Person('', 'Nahuel', new Date(2002, 6, 25), ''),
+        new Person('', 'Aaron', new Date(2002, 6, 25), ''),
+        new Person('', 'Rodripollera', new Date(2002, 7, 9), ''),
+        new Person('', 'Elias', new Date(2002, 7, 28), ''),
+        new Person('', 'X', new Date(2002, 8, 7), ''),
+        new Person('', 'Shadow', new Date(2002, 8, 18), ''),
+        new Person('', 'Seba', new Date(2006, 9, 31), ''),
+        new Person('', 'Alexander', new Date(2000, 11, 3), ''),
+        new Person('', 'Maty', new Date(2000, 2, 11), '')
+    ]
     addPerson (person: IPerson): void {
         this.persons = [...this.persons, person ]
     }
